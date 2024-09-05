0820edd67cc0eb945ee979bc74ca110c04c91a63
Satwik Gupta <satwikgupta1801@gmail.com>
2024-09-05 13:17:07 +0530

    chore: Refactor email template for contact form submission

--- src/app/api/sendmail/_mail.ts
+++ src/app/api/sendmail/_mail.ts
@@ -9,213 +9,37 @@ const mailler = ({
   message,
 }: ContactFormData) => {
   return `
-  <html
-  dir="ltr"
-  xmlns="http://www.w3.org/1999/xhtml"
-  xmlns:o="urn:schemas-microsoft-com:office:office"
->
-  <link type="text/css" rel="stylesheet" id="dark-mode-custom-link" />
-  <link type="text/css" rel="stylesheet" id="dark-mode-general-link" />
-  <style lang="en" type="text/css" id="dark-mode-custom-style"></style>
-  <style lang="en" type="text/css" id="dark-mode-native-style"></style>
-  <style lang="en" type="text/css" id="dark-mode-native-sheet"></style>
-
-  <head>
-    <meta charset="UTF-8" />
-    <meta content="width=device-width, initial-scale=1" name="viewport" />
-    <meta name="x-apple-disable-message-reformatting" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta content="telephone=no" name="format-detection" />
-    <title></title>
-  </head>
-  <body>
-    <div dir="ltr" className="es-wrapper-color">
-      <table className="es-wrapper" width="100%" cellSpacing="0" cellPadding="0">
-        <tbody>
-          <tr>
-            <td className="esd-email-paddings" valign="top">
-              <table
-                className="es-content esd-header-popover"
-                cellSpacing="0"
-                cellPadding="0"
-                align="center"
-              >
-                <tbody>
-                  <tr>
-                    <td
-                      className="esd-stripe"
-                      esd-custom-block-id="2123"
-                      align="center"
-                    >
-                      <table
-                        className="es-content-body"
-                        width="600"
-                        cellSpacing="0"
-                        cellPadding="0"
-                        bgcolor="#ffffff"
-                        align="center"
-                      >
-                        <tbody>
-                          <tr>
-                            <td
-                              className="esd-structure es-p5t es-p5b"
-                              esd-general-paddings-checked="false"
-                              align="left"
-                            >
-                              <table
-                                width="100%"
-                                cellSpacing="0"
-                                cellPadding="0"
-                              >
-                                <tbody>
-                                  <tr>
-                                    <td
-                                      className="es-m-p0r esd-container-frame"
-                                      width="124"
-                                      valign="top"
-                                      align="center"
-                                    >
-                                      <a
-                                        target="_blank"
-                                        href="mailto:satwikgupta1801@gmail.com"
-                                      >
-                                        <img
-                                          src="https://suryanshvermon.github.io/my-portfolio/assets/images/avatar-4.png"
-                                          alt="Satwik Gupta"
-                                          title="Satwik Gupta"
-                                          style="display: block"
-                                          width="61"
-                                        />
-                                      </a>
-                                    </td>
-                                    <td
-                                      className="esd-container-frame"
-                                      width="456"
-                                      align="left"
-                                    >
-                                      <span
-                                        style="
-                                          color: #1e312b;
-                                          font-weight: normal;
-                                          font-size: 26px;
-                                        "
-                                      >
-                                        Your portfolio is accessed by
-                                        ${firstname}
-                                      </span>
-                                    </td>
-                                  </tr>
-                                </tbody>
-                              </table>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <table
-                                width="100%"
-                                cellSpacing="0"
-                                cellPadding="0"
-                                style="
-                                  border-collapse: collapse;
-                                  margin-bottom: 20px;
-                                "
-                              >
-                                <tr>
-                                  <th
-                                    style="
-                                      padding: 10px;
-                                      text-align: left;
-                                      background-color: #f2f2f2;
-                                      border: 1px solid #ddd;
-                                    "
-                                  >
-                                    Name
-                                  </th>
-                                  <td
-                                    style="
-                                      padding: 10px;
-                                      border: 1px solid #ddd;
-                                    "
-                                  >
-                                    ${firstname} ${lastname}
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <th
-                                    style="
-                                      padding: 10px;
-                                      text-align: left;
-                                      background-color: #f2f2f2;
-                                      border: 1px solid #ddd;
-                                    "
-                                  >
-                                    Email
-                                  </th>
-                                  <td
-                                    style="
-                                      padding: 10px;
-                                      border: 1px solid #ddd;
-                                    "
-                                  >
-                                    ${email}
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <th
-                                    style="
-                                      padding: 10px;
-                                      text-align: left;
-                                      background-color: #f2f2f2;
-                                      border: 1px solid #ddd;
-                                    "
-                                  >
-                                    Phone No.
-                                  </th>
-                                  <td
-                                    style="
-                                      padding: 10px;
-                                      border: 1px solid #ddd;
-                                    "
-                                  >
-                                    ${phone}
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <th
-                                    style="
-                                      padding: 10px;
-                                      text-align: left;
-                                      background-color: #f2f2f2;
-                                      border: 1px solid #ddd;
-                                      vertical-align: top;
-                                    "
-                                  >
-                                    Message
-                                  </th>
-                                  <td
-                                    style="
-                                      padding: 10px;
-                                      border: 1px solid #ddd;
-                                      word-wrap: break-word;
-                                    "
-                                  >
-                                    ${message}
-                                  </td>
-                                </tr>
-                              </table>
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div> </body>`;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact Form Submission</title>
+</head>
+<body>
+  <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto;">
+    <h1 style="color: #1e312b;">Your portfolio is accessed by ${firstname}</h1>
+    <table style="width: 100%; border-collapse: collapse; margin-bottom: 20px;">
+      <tr>
+        <th style="padding: 10px; text-align: left; background-color: #f2f2f2; border: 1px solid #ddd;">Name</th>
+        <td style="padding: 10px; border: 1px solid #ddd;">${firstname} ${lastname}</td>
+      </tr>
+      <tr>
+        <th style="padding: 10px; text-align: left; background-color: #f2f2f2; border: 1px solid #ddd;">Email</th>
+        <td style="padding: 10px; border: 1px solid #ddd;">${email}</td>
+      </tr>
+      <tr>
+        <th style="padding: 10px; text-align: left; background-color: #f2f2f2; border: 1px solid #ddd;">Phone No.</th>
+        <td style="padding: 10px; border: 1px solid #ddd;">${phone}</td>
+      </tr>
+      <tr>
+        <th style="padding: 10px; text-align: left; background-color: #f2f2f2; border: 1px solid #ddd; vertical-align: top;">Message</th>
+        <td style="padding: 10px; border: 1px solid #ddd; word-wrap: break-word;">${message}</td>
+      </tr>
+    </table>
+  </div>
+</body>
+</html>`;
 };
 
 export default mailler;
